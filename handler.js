f56cd4a6806aedc2ddfb9af88f006f3d74d362cd
danie16arrido <daniel.garrido.info@gmail.com>
2017-11-23 12:09:27 +0000

    npm i request-promise + use authorizer in handler

--- handler.js
+++ handler.js
@@ -1,15 +1,35 @@
 'use strict';
+const authorizer = require('./authorizer');
 
 module.exports.authorization = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Authorization was called!',
-      input: event,
-    }),
-  };
+  const code = event.queryStringParameters.code;
+  console.log(code);
 
-  callback(null, response);
+  authorizer(code)
+    .then(() => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Authorization was called',
+          input: event,
+        }),
+      };
+
+      callback(null, response);
+    })
+    .catch((error) => {
+      console.log("Authentication error Slack's OAuth");
+
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: error,
+          input: event,
+        }),
+      };
+
+      callback(null, response);
+    });
 };
 
 module.exports.git_statz = (event, context, callback) => {
