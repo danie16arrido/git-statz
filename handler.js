063fde196932b4a3208e3b13ec8f768459fc4d34
danie16arrido <daniel.garrido.info@gmail.com>
2017-11-23 11:55:43 +0000

    create git_statz in handler

--- handler.js
+++ handler.js
@@ -10,7 +10,16 @@ module.exports.authorization = (event, context, callback) => {
   };
 
   callback(null, response);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.git_statz = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'git statz was called!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
 };
