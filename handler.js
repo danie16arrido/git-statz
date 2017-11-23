8578987cd61c8807ed81723aa7ed5200bd43ee1c
danie16arrido <daniel.garrido.info@gmail.com>
2017-11-23 11:43:30 +0000

    create authorization function

--- handler.js
+++ handler.js
@@ -1,10 +1,10 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.authorization = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Authorization was called!',
       input: event,
     }),
   };
